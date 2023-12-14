fdf9646d8b9b25f426f3c68a665926cc76b0f1ef
priyanshu kumar <dlnpriyanshu@gmail.com>
2023-12-14 10:10:45 +0530

    forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Navbar from '../src/components/navbar.js';
-import Footer from './components/footer.js';
+import Home from './components/home';
+import {
+  Route,
+  Routes,
+} from "react-router-dom";
+import LoginForm from './components/loginform.js';
+import Signup from './components/signup.js';
+// import LoginForm from './LoginForm';
+// import SignupForm from './SignupForm';
+
 function App() {
   return (
-  <>
-    <Navbar />
-   <br/>
-   <br/>
-   <br/>
-    <Footer />
-  </>
+ <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/login' element={<LoginForm />}/>
+    <Route path='/signup' element={<Signup />}/>
+ </Routes>
   );
 }
 
